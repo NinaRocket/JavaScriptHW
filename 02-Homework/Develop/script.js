b827dcfeaa25a6e0f41ac4a7902e5dedd8347432
NRocket <ninamrocket@gmail.com>
2020-04-18 20:34:28 -0700

    password generator works

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -21,7 +21,8 @@ var u = userUpperCase;
 
 var pw = "";
 var generate = true;
-
+var criteriaArray = [];
+var password1 = [];
 
 var pwLength = prompt("How many characters long does your password need to be? Please choose between 8 and 128 characters.")
 
@@ -29,44 +30,55 @@ if (pwLength < 8 || pwLength > 128) {
 
 
   alert("Your password must be between 8 and 128 characters");
-  generate = false;
+  //generate === false;
 }
 
 
 var usChar = prompt("Please enter s if you need a special characters for your password, press enter to continue.");
 
 if (usChar === "s") {
-  // pw.push(userSpecial);
-  // console.log.apply(pw);
-  pw += s;
-}
-var usChar = prompt("Please enter l if you need a special characters for your password, press enter to continue.");
-if (usChar === "l") {
-  // pw.push(l);
-  pw += l;
-}
-var usChar = prompt("Please enter u if you need a special characters for your password, press enter to continue.");
-if (usChar === "u") {
-  // pw.push(u);
-  pw += u;
-}
-var usChar = prompt("Please enter n if you need a special characters for your password, press enter to continue.");
-if (usChar === "n") {
-  // pw.push(n);
-  pw += n;
-}
 
+  criteriaArray.push(s.charAt([Math.floor(Math.random() * s.length)]));
+  pw += s;
+  console.log(criteriaArray);
+}
+var usChar = prompt("Please enter l if you need a lowercase characters for your password, press enter to continue.");
+if (usChar === "l") {
+
+  criteriaArray.push(l.charAt([Math.floor(Math.random() * l.length)]));
+  pw += l;
+  console.log(criteriaArray);
+}
+var usChar = prompt("Please enter u if you need a uppercase characters for your password, press enter to continue.");
+if (usChar === "u") {
+
+  criteriaArray.push(u.charAt([Math.floor(Math.random() * u.length)]));
+  pw += u;
+  console.log(criteriaArray);
+}
+var usChar = prompt("Please enter n if you need a number for your password, press enter to continue.");
+if (usChar === "n") {
+
+  criteriaArray.push(n.charAt([Math.floor(Math.random() * n.length)]));
+  pw += n;
+  console.log(criteriaArray);
+}
 
 
 for (i = 0; i < pwLength; i++) {
-  password += pw.charAt([Math.floor(Math.random() * pw.length)]);
+  password1 += pw.charAt([Math.floor(Math.random() * pw.length)]);
 
 }
 
-console.log(password);
+var finalPW = password1.split("");
+
+
+Array.prototype.splice.apply(finalPW, [0, criteriaArray.length].concat(criteriaArray));
+
+
+console.log(password1);
 console.log(pw);
-
-
+console.log(finalPW);
 
 
 // Add event listener to generate button
